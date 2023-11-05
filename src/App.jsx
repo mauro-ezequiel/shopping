ea887e9a6b24908b17e5000699ce1a2ac2d05866
mauro-ezequiel <129565868+mauro-ezequiel@users.noreply.github.com>
2023-11-05 20:15:11 -0300

    Add bootstrap and bulma, connect project files.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Product } from "./components/products";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hola 123</h1>
+        <Product />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/products.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Product() {
+  return (
+    <section>
+      <h1>product</h1>
+    </section>
+  );
+}
