eaa43a52b0b7ac8b1b18a4e18587d3608f1b52c3
mauro-ezequiel <129565868+mauro-ezequiel@users.noreply.github.com>
2023-11-05 20:43:35 -0300

    descomprimi a la api

---
+++ src/components/pages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Pages() {
+  return (
+    <section>
+      <div>
+        <h1>pages</h1>
+      </div>
+    </section>
+  );
+}
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { Pages } from "./pages";
+import { useState } from "react";
 
 export function Product() {
+  const [products, setProducts] = useState([]);
+
+  const [proPorPage, setProPorPage] = useState(4);
+
+  const [curentP, setCurentP] = useState(1);
+
+  const productList = async () => {
+    const data = await fetch("https://fakestoreapi.com/products");
+
+    const products = await data.json();
+    console.log(products);
+
+    setProducts(products);
+  };
+
+  productList();
+
   return (
     <section>
-      <h1>product</h1>
+      <Pages />
     </section>
   );
 }
